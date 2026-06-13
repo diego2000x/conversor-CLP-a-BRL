003fca0f185fd2f4b84b1c0de6ee692bea9eed3d
Diego Ramirez <diegoramirez2004@gmail.com>
2026-06-13 13:18:30 -0400

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Conversor CLP BRL",
+        short_name: "Conversor",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "icono.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
